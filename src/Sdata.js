9525e0831b1e3bbc1aa50a0d70f1149f927f6a14
sejwalRitika <ritikasejwal330@gmail.com>
2023-08-10 12:53:35 +0530

    first commit

---
+++ src/Sdata.js
@@ -0,0 +1,40 @@
+const Sdata = [
+{
+sname: "DARK",
+imgsrc: "https://1.bp.blogspot.com/-eqjPhfRdL-M/X2hyoK5cOPI/AAAAAAAAQwg/tFQ2zpIikAwW2Yh-APt4IMjJKVUrpgLywCLcBGAsYHQ/s849/b890a863026055.5aa3225669f72.webp",
+title: "Netflix Original Series",
+link: "https://www.netflix.com/in/title/80100172",
+},
+{
+sname: "extra curricular",
+imgsrc: "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+title: "Netflix Original Series",
+link: "https://www.netflix.com/in/title/80990668?source=35",
+},
+{
+sname:"Stranger Things",
+imgsrc: "https://resizing.flixster.com/0xxuABVVuzJrUT130WFHKE-irEg=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vNzUyMTFhOTktZTU4Ni00ODkyLWJlYjQtZTgxYTllZmU2OGM0LmpwZw==",
+title: "Netflix Original Series",
+link: "https://www.netflix.com/in/title/80057281",
+},
+{
+sname:"The vampire Diaries",
+imgsrc: "https://flxt.tmsimg.com/assets/p13004312_b_v9_au.jpg",
+title: "Azon Original Series",
+link: "https://www.netflix.com/in/title/70143860",
+},
+{
+sname:"My first - 2 love",
+imgsrc: "https://m.media-amazon.com/images/M/MV5BMWVhNTgwMWEtN2U4Yi00NDhhLWIxOWQtNDc2Y2Y1MTFmM2U1XkEyXkFqcGdeQXVyMTEwMTcyMzkz._V1_FMjpg_UX1000_.jpg",
+title: "Netflix Original Series",
+link: "https://www.netflix.com/in/title/81026700",
+},
+{
+sname: "1899",
+imgsrc: "https://m.media-amazon.com/images/M/MV5BMTU1N2I5OTgtNDE2MS00ZTdmLWJlODktNDMwMGVmNzY5NmUxXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+title: "Netflix Original Series",
+link: "https://www.netflix.com/in/title/80214497",
+},
+]
+
+export default Sdata;
